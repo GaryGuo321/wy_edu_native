c8f487ed468e8b1c3a70480b4d292d29fa4ea16d
GaryGuo321 <648797223@qq.com>
2016-06-24 15:37:18 +0800

    modify ie event

--- js/myLibrary.js
+++ js/myLibrary.js
@@ -32,12 +32,17 @@ data: 2016.4.25
 	}
 	// bind兼容
 	if (!Function.prototype.bind) {
-		Function.prototype.bind = function(context) {
-			var self = this;
-			var args = Array.prototype.slice.call(arguments);
-			return function() {
-				return self.apply(context, args.slice(1));
-			}
+		Function.prototype.bind = function(oThis) {
+			var aArgs = Array.prototype.slice.call(arguments, 1),
+				fToBind = this,
+				fNOP = function() {},
+				fBound = function() {
+					return fToBind.apply(this instanceof fNOP && oThis ? this : oThis,
+						aArgs.concat(Array.prototype.slice.call(arguments)));
+				};
+			fNOP.prototype = this.prototype;
+			fBound.prototype = new fNOP();
+			return fBound;
 		};
 	}
 }();
